069c232358316ba11dcec7aaf111b8aec4cdc046
Ivan-Vanzhula <ivan.vanzula@knu.ua>
2024-05-31 11:22:18 +0300

    create tests for models

---
+++ src/tests/test_models.py
@@ -0,0 +1,47 @@
+import pytest
+from online_shop.mvc.models import *
+
+@pytest.mark.django_db
+def test_product_create():
+    product = Product.objects.create(
+        name="Test Product",
+        description="Test Description",
+        price=99.99,
+        image="products/test.jpg",
+        rating=5,
+        specifications="Test Specifications"
+    )
+    assert product.name == "Test Product"
+    assert product.description == "Test Description"
+    assert product.price == 99.99
+    assert product.image == "products/test.jpg"
+    assert product.rating == 5
+    assert product.specifications == "Test Specifications"
+
+@pytest.mark.django_db
+def test_cart_creation():
+    cart = Cart.objects.create(cart_id="test_cart")
+    assert cart.cart_id == "test_cart"
+
+@pytest.mark.django_db
+def test_cart_item_creation():
+    product = Product.objects.create(
+        name="Test Product",
+        description="Test Description",
+        price=99.99,
+        image="products/test.jpg",
+        rating=5,
+        specifications="Test Specifications"
+    )
+    cart = Cart.objects.create(cart_id="test_cart")
+    cart_item = CartItem.objects.create(
+        product=product,
+        cart=cart,
+        quantity=2,
+        is_active=True
+    )
+    assert cart_item.product == product
+    assert cart_item.cart == cart
+    assert cart_item.quantity == 2
+    assert cart_item.is_active == True
+    assert cart_item.sub_total() == 199.98
