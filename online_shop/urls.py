e2bad2d8f78a1be550c5cfb94c8ffb35450342e7
Zlatous-Illia <zlatous_illia@knu.ua>
2024-06-04 14:52:12 +0300

    Changing templates

--- online_shop/urls.py
+++ online_shop/urls.py
@@ -19,7 +19,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from mvc.views.views import *  # Adjusted import statement
+from shop.views import *  # Adjusted import statement
 from django.conf import settings
 from django.conf.urls.static import static
 
