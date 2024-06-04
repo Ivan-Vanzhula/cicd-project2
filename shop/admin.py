52a8fc980fda53bf53974b12cc357e8993a6adad
Ivan-Vanzhula <ivan.vanzula@knu.ua>
2024-06-04 18:49:55 +0300

    test cicd

--- shop/admin.py
+++ shop/admin.py
@@ -3,6 +3,7 @@
 from django.contrib import admin
 from shop.models import Product  # оновлено імпорт
 
+#for manage products
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'price')
